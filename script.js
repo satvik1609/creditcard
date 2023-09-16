a4d6519fb941ac2e5c1d5ec80526438dd24167b4
Satvik Srivastava <112679866+satvik1609@users.noreply.github.com>
2023-09-16 23:31:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the child1 and child2 elements
+  const child1 = document.querySelector(".child1");
+  const child2 = document.querySelector(".child2");
+
+  // Add a class to start the animation
+  child1.classList.add("rotate");
+  child2.classList.add("rotate");
+});
